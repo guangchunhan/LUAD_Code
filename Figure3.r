08cf20de2b3a9fde44db825252cecb9172123d94
guangchunhan <ghan1@mdanderson.org>
2022-12-14 18:49:26 -0600

    codes updates for Fig2-4

--- Figure3.r
+++ Figure3.r
@@ -1,31 +1,54 @@
-##figure3 b generated using jmp with the following data
+##figure3 b
 if(TRUE) {
-    fractionData = read_tsv('/Users/ghan1/Library/CloudStorage/Box-Box/Humam/mouse_data_1006/Grant figures2021 Sept/scripts/Fig1D data barplots of fraction.jmp')
-    fig3b = ggplot(data = fractionData)+
-    geom_boxplot(aes(x = `treatment`,y=Fraction), 
-                         width = .3,
+    library(ggplot2)
+    fractionData = read_tsv('Input_Data/Fig3_FractionChange_dataInput.txt')
+    fractionData$treatment = factor(fractionData$treatment,
+                                    levels = c('Saline','NNK'))
+
+fractionData$timepoint = factor(fractionData$timepoint,
+                                    levels = c('EOE','7 Mo'))
+
+    fig3b_left = ggplot(data = filter(fractionData,celltype == 'Malignant'),
+                        aes(x = `treatment`,y=Fraction))+
+    geom_boxplot( width = .3,
                  alpha = .3,outlier.shape= NA)  +
-        geom_jitter() +
+        geom_point() +
     xlab('') +
-    theme_classic() + facet_grid(`Column 18`~timepoint) + ##Column 18 contains the most recent celltype information
-    theme(
+    theme_classic() + facet_wrap(~timepoint) + theme(
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         legend.position='none',
         axis.text.x = element_text(angle=0)
     ) 
+    print(fig3b_left)
+
+
+fig3b_right = ggplot(data = filter(fractionData,celltype == 'KAC'),
+                        aes(x = `treatment`,y=Fraction))+
+    geom_boxplot( width = .3,
+                 alpha = .3,outlier.shape= NA)  +
+        geom_point() +
+    xlab('') +
+    theme_classic() + facet_wrap(~timepoint) + theme(
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position='none',
+        axis.text.x = element_text(angle=0)
+    ) 
+    print(fig3b_right)
+
 }
 
-##figure3 d was generated using jmp v15 with umap 2d dimention reduction data loaded.
+##figure3d was generated using JMP Pro v15 with umap 2d dimention reduction data loaded.
 
 ##figure3 e violinplots of cnv scores and Kras mutation fration barplot
+##Figure3 e
 if(TRUE) {
-        cnvdata = read_tsv('/Users/ghan1/Library/CloudStorage/Box-Box/Humam/LUAD(P1-P16)/figures/figures_data/epithelial/EpithelialUmap/Epithelial_normalCells/alveolar_airway_andAllfiguresworkdir/Figure4 mousedata cnvscore.jmp')
-    fig3etop = ggplot(data = cnvdata)+
-    geom_violin(aes(x = `Column 18`,y=aviv_score_refT), 
-                         width = .3,
-                 alpha = .3,outlier.shape= NA)  +
-        geom_boxplot(width = .2) +
+        cnvdata = read_tsv('Input_data/Fig3e_cnv_dataInput.txt')
+    fig3etop = ggplot(data = cnvdata,aes(x = `celltype`,y=cnvscore))+
+        geom_jitter(width = .2) +
+        geom_violin(scale = 'width',width=.5,
+                    alpha = .8,outlier.shape= NA)  +
     xlab('') +
     theme_classic() +
     theme(
@@ -34,14 +57,12 @@ if(TRUE) {
         legend.position='none',
         axis.text.x = element_text(angle=0)
     ) 
+        print(fig3etop)
 
-
-        ##bottome part of the panel showing the fraction of G12D mutaiton cell fraction
-        data = read_excel('SuppTable.xlsx',sheet = 11)
-        fig3ebottom = ggplot(data = data)+
-    geom_bar(aes(x = `Group`,y=Fraction), 
-                         width = .3)
-        geom_label(`number of cells with Kras G12D mutation`) + 
+        ##bottome part of the panel showing the fraction of G12D mutaiton cell fraction(Data from supplementary Table S11)
+        data = read_tsv('Input_Data/Fig3e_KrasFraction_dataInput.txt')
+        fig3ebottom = ggplot(data = data,aes(x = `Group`,y=Fraction))+
+    geom_bar(width = .3,stat = 'identity') +
     xlab('') +
     theme_classic() +
     theme(
@@ -50,5 +71,6 @@ if(TRUE) {
         legend.position='none',
         axis.text.x = element_text(angle=0)
     ) 
+        print(fig3ebottom)
 }
 
